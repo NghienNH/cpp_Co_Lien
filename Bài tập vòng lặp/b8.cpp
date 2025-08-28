d613764b30ecca97cec720981504809a52813055
NghienNH <nghien3101@gmail.com>
2025-08-28 10:48:58 +0700

    Update b8.cpp

--- Bài tập vòng lặp/b8.cpp
+++ Bài tập vòng lặp/b8.cpp
@@ -2,25 +2,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int prime(int x) {
-    if (x < 2) return false;
-    for (int i = 2; i * i <= x; i++) {
-        if (x % i == 0) return false;
-    }
-    return true;
-}
-
 int main() {
     int n;
     cin >> n;
+    set<int> se;
     for (int i = 2; i * i <= n; i++) {
         if (n % i == 0) {
-            if (prime(i)) cout << i << " ";
-            if (i != n / i && prime(n / i)) cout << n / i << " ";
+            se.insert(i);
+            se.insert(n/i);
         }
     }
-    if (prime(n)) cout << n; 
-    cout << endl;
-
+    for(int divisor : se){
+        cout << divisor << " ";
+    }
     return 0;
 }

a378fa4da8a040ddb9f3ded19a7c9dfe3e7d7531
NghienNH <nghien3101@gmail.com>
2025-08-28 09:51:58 +0700

    Update b9.cpp

--- Bài tập câu lệnh rẽ nhánh/b9.cpp
+++ Bài tập câu lệnh rẽ nhánh/b9.cpp
@@ -1,1 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a,b; cin>>a>>b;
+    char c; cin>>c;
+    if (c=='+') cout <<a+b;
+    else if (c=='-') cout <<a-b;
+    else if (c=='*') cout <<a*b;
+    else if (c=='/') cout <<a/b;
+}
 
